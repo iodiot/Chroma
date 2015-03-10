f445bf57750590f5897fd589175768f8c1aa0999
13x666 <gordeyev.y@gmail.com>
2015-03-10 23:02:22 +0200

    [Part of previous commit]

---
+++ Chroma/Actors/FlatPlatformActor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Chroma.Graphics;
+using Chroma.Messages;
+using Chroma.Helpers;
+using Chroma.Gameplay;
+
+namespace Chroma.Actors
+{
+  public abstract class FlatPlatformActor : PlatformActor
+  {
+    public int width { get; protected set; }
+    protected Sprite floorSprite;
+
+    public FlatPlatformActor(Core core, Vector2 position, int width, Area area) : base(core, position, area)
+    {
+      this.width = width;
+      GetSprites();
+
+      if (this.width % floorSprite.Width > 0)
+      {
+        this.width -= this.width % floorSprite.Width;
+        this.width += floorSprite.Width;
+      }
+
+      boundingBox = new Rectangle(0, 0, this.width, 300);
+      LeftY = RightY = (int)position.Y;
+
+      CanMove = false;
+      CanFall = true;
+    }
+
+    protected virtual void GetSprites() {
+    }
+
+    public override void Draw()
+    {
+      DrawEdges();
+      DrawGround();
+
+      base.Draw();
+    }   
+
+    public static FlatPlatformActor Create(Core core, Vector2 position, int width, Area area)
+    {
+      switch (area)
+      {
+        default:
+        case Area.Jungle:
+          return new JungleFlatPlatformActor(core, position, width, area);
+        case Area.Ruins:
+          return new RuinsFlatPlatformActor(core, position, width, area);
+
+      }
+    }
+
+    protected abstract void DrawGround();
+  }
+}
+
---
+++ Chroma/Actors/JungleFlatPlatformActor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Chroma.Graphics;
+using Chroma.Messages;
+using Chroma.Helpers;
+using Chroma.Gameplay;
+
+namespace Chroma.Actors
+{
+  public class JungleFlatPlatformActor : FlatPlatformActor
+  {
+    private Sprite grassSprite;
+
+    protected override void GetSprites()
+    {
+      floorSprite = core.SpriteManager.GetSprite("floor");
+      grassSprite = core.SpriteManager.GetSprite("floor_grass");
+
+      base.GetSprites();
+    }
+
+    public JungleFlatPlatformActor(Core core, Vector2 position, int width, Area area) : base(core, position, width, area)
+    {
+      // Vines
+      if (ScienceHelper.ChanceRoll(0.5f))
+      {
+        var x = Position.X + ScienceHelper.GetRandom(4, width - 12);
+        var vine = ScienceHelper.GetRandom(1, 4);
+        core.MessageManager.Send(new AddActorMessage(new DecalActor(core, 
+          new Vector2(x, Position.Y),
+          "vine_" + vine.ToString(),
+          flip: ScienceHelper.ChanceRoll(0.5f),
+          depth: 10
+        )), this);
+        if (ScienceHelper.ChanceRoll(0.3f))
+        {
+          x += 8;
+          var vine2 = ScienceHelper.GetRandom(1, 4);
+          while (vine == vine2)
+          {
+            vine2 = ScienceHelper.GetRandom(1, 4);
+          }
+          core.MessageManager.Send(new AddActorMessage(new DecalActor(core, 
+            new Vector2(x, Position.Y),
+            "vine_" + vine2.ToString(),
+            flip: ScienceHelper.ChanceRoll(0.5f),
+            depth: 10
+          )), this);
+        }
+      }
+
+      // Boulders
+      if (ScienceHelper.ChanceRoll(0.8f))
+      {
+        var boulder = ScienceHelper.GetRandom(1, 4);
+        var x = Position.X + ScienceHelper.GetRandom(4, width - 4 - 30);
+        core.MessageManager.Send(new AddActorMessage(new DecalActor(core, 
+          new Vector2(x, Position.Y + ScienceHelper.GetRandom(15, 60)),
+          "earth_boulder_" + boulder.ToString()
+        )), this);
+      }
+    }
+
+    protected override void DrawGround()
+    {
+      var n = width / floorSprite.Width;
+      for (var i = 0; i <= n; ++i)
+      {
+        core.Renderer.DrawSpriteW(
+          i == n ? floorSprite.ClampWidth(width % floorSprite.Width) : floorSprite, 
+          new Vector2(Position.X + floorSprite.Width * i, Position.Y - 13)
+        );
+        core.Renderer[50].DrawSpriteW(
+          i == n ? grassSprite.ClampWidth(width % grassSprite.Width) : grassSprite, 
+          new Vector2(Position.X + grassSprite.Width * i, Position.Y - 6)
+        );
+      }
+
+    }
+  }
+}
+
